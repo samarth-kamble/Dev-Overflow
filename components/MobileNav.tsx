baa5d3aabb906aa756d8f00f5b98011fbaae25b3
Samarth Kamble <samarthkamble@Samarths-MacBook-Air.local>
2024-07-12 19:10:47 +0530

    Implement the mobilenav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,7 +1,78 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
 import React from "react";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet";
+import Link from "next/link";
+import Image from "next/image";
+import { Menu } from "lucide-react";
+
+const NavContent = () => {
+  const pathname = usePathname();
+  return (
+    <section className="mb-2 flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map((item) => {
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname === item.route;
+
+        return (
+          <SheetClose asChild key={item.route}>
+            <Link
+              href={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : " text-dark300_light900 "
+              } flex items-center justify-start gap-4 bg-transparent p-4 `}
+            >
+              <Image
+                className={`${isActive ? "" : "invert-colors"}`}
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {item.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
+};
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Menu className="h-6 w-6 cursor-pointer text-black sm:hidden dark:text-white" />
+      </SheetTrigger>
+      <SheetContent
+        side={"left"}
+        className="background-light900_dark200 scrollbar-hidden overflow-y-auto border-none"
+      >
+        <Link href={"/"} className="flex items-center gap-1">
+          <Image
+            src={"/assets/images/site-logo.svg"}
+            width={23}
+            height={23}
+            alt="Dev-Overflow"
+          />
+          <p className="h2-bold text-dark100_light900 ml-2 font-spaceGrotesk">
+            Dev<span className="text-primary-500">OverFlow</span>
+          </p>
+        </Link>
+        <div>
+          <SheetClose asChild>
+            <NavContent />
+          </SheetClose>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
